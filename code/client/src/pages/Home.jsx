e5c38215aeee4d7bd451c5a03c5ace79b0e1dba7
Matvey <hodos.952795@gmail.com>
2023-10-24 00:24:44 +0300

    init react app

---
+++ code/client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Typography} from "@mui/material";
+
+const Home = () => {
+
+    return(
+        <Typography>
+            hello Matvey
+        </Typography>
+    )
+
+}
+
+export default Home;
---
+++ code/client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+}
